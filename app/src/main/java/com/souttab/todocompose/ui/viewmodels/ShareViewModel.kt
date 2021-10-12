ce92257989ed02b554e62c79daf01dd16576a560
souttab <andiyulistyo@gmail.com>
2021-10-12 22:42:13 +0700

    add shareviewmodel

--- app/src/main/java/com/souttab/todocompose/ui/viewmodels/ShareViewModel.kt
+++ app/src/main/java/com/souttab/todocompose/ui/viewmodels/ShareViewModel.kt
@@ -1,7 +1,27 @@
 package com.souttab.todocompose.ui.viewmodels
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.souttab.todocompose.data.models.ToDoTasks
+import com.souttab.todocompose.data.repository.TodoRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class ShareViewModel() : ViewModel() {
+@HiltViewModel
+class ShareViewModel @Inject constructor(private val repository: TodoRepository) : ViewModel() {
 
+    private val _allTasks = MutableStateFlow<List<ToDoTasks>>(emptyList())
+    val allTasks : StateFlow<List<ToDoTasks>> = _allTasks
+
+    fun getAllTask() {
+        viewModelScope.launch {
+            repository.getAllTasks.collect {
+                _allTasks.value = it
+            }
+        }
+    }
 }
